aa1641dccaab6fb83fd781a84f55593f06295782
HitTheStars <1272063565@qq.com>
2025-07-30 01:57:05 +0800

    刚刚学会了python的csv读取功能

--- calculating.py
+++ calculating.py
@@ -1,4 +1,10 @@
 import opendatasets as od
-iris = od.download("https://www.kaggle.com/datasets/uciml/iris")
+import numpy as np
+import csv
+
+with open('G:\my_lovely_codes\math_model\iris\Iris.csv', 'w', newline='') as iris_file:
+    writer = csv.writer(iris_file)
+    writer.writerow(['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
+
 print("Iris dataset downloaded successfully.")
 print("Dataset location:", iris)
