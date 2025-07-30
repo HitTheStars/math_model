25761aef12d99322b8ce05c9b329818e812a9897
HitTheStars <1272063565@qq.com>
2025-07-31 00:28:09 +0800

    增加了学习numpy的notebook

--- day1_python_review/calculating.py
+++ day1_python_review/calculating.py
@@ -1,5 +1,5 @@
 import opendatasets as od
-import numpy as np
+
 import csv
 
 iris=od.download("https://www.kaggle.com/datasets/uciml/iris")
@@ -20,12 +20,12 @@ for line in lines[1:]:            # lines[0]是标题行，跳过
     petal_lengths.append(petal_length)
 print("花瓣长度列表:", petal_lengths)
 print("前5个花瓣长度:", petal_lengths[:5])
-print("花瓣长度的平均值:", np.mean(petal_lengths))
-print("花瓣长度的标准差:", np.std(petal_lengths))
-print("花瓣长度的最大值:", np.max(petal_lengths))
-print("花瓣长度的最小值:", np.min(petal_lengths))       
-print("花瓣长度的中位数:", np.median(petal_lengths))    
-print("花瓣长度的方差:", np.var(petal_lengths))
-print("花瓣长度的四分位数:", np.percentile(petal_lengths, [25, 50, 75]))  # 计算四分位数                                
+print("花瓣长度的平均值:", sum(petal_lengths)/len(petal_lengths))
+print("花瓣长度的标准差:", sum((x - sum(petal_lengths)/len(petal_lengths))**2 for x in petal_lengths)/len(petal_lengths))
+print("花瓣长度的最大值:", max(petal_lengths))
+print("花瓣长度的最小值:", min(petal_lengths))       
+print("花瓣长度的中位数:", sorted(petal_lengths)[len(petal_lengths)//2])    
+print("花瓣长度的方差:", sum((x - sum(petal_lengths)/len(petal_lengths))**2 for x in petal_lengths)/len(petal_lengths))
+print("花瓣长度的四分位数:", sorted(petal_lengths)[len(petal_lengths)//4],sorted(petal_lengths)[len(petal_lengths)//2],sorted(petal_lengths)[3*len(petal_lengths)//4])  # 计算四分位数                                
 print("花瓣宽度列表:", petal_widths)
-print("花瓣宽度平均值：",np.mean(petal_widths))
+print("花瓣宽度平均值：",sum(petal_widths)/len(petal_widths))
